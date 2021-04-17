3308b6e501b483b8162264988fe769a5fec46292
John Blalock <simpledimplejohn@gmail.com>
2021-04-17 08:17:20 -0700

    changed names to debug jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,6 @@
 $(document).ready(function() {
-
-  $("#one").click(function() {
-    $("#one").prepend("<p>one</p>");
-    $("box").removeClass();
-    $("box").addClass("white-background")
-
-  })
-
-})
-  
+  $(".clickable").click(function() {
+    $(".boxfront").toggle();
+    $(".boxback").toggle();
+  });
+});
